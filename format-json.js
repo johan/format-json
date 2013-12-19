0e9f22a6c9ed3aa2b01237dc9833e925d9650d91
Johan Sundström <jsundstrom@groupon.com>
2013-12-18 16:59:52 -0800

    Added format-json <files> shell tool

---
+++ format-json.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+var fs   = require('fs')
+  , json = require('./index')
+  , args = process.argv.slice(2)
+  , data = []
+  , stdin = process.stdin
+  , status
+  ;
+
+function log(raw) {
+  try {
+    process.stdout.write(json.diffy(JSON.parse(raw)));
+  } catch(e) { return 1; }
+  return 0;
+}
+
+if (args.length) {
+  status = args.map(function(name) { log(fs.readFileSync(name, 'utf8')); });
+  process.exit(status.reduce(function(a, b) { return a + b; }, 0));
+}
+else {
+  stdin.resume();
+  stdin.setEncoding('utf8')
+  stdin.on('data', function(chunk) { data.push(chunk); })
+  stdin.on('end', function() { process.exit(log(data.join(''))); });
+}
